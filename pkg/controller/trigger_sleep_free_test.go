cb5e874c027f4e999f97cb76f544046700fda7a7
Ryan Gaffney <ryanmgaffney@gmail.com>
2026-08-19 16:22:22 -0400

    test(controller): reject sleep in trigger regression

---
+++ pkg/controller/trigger_sleep_free_test.go
@@ -0,0 +1,31 @@
+package controller_test
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"testing"
+)
+
+func TestTriggerRegressionsDoNotSleep(t *testing.T) {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "trigger_test.go", nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ast.Inspect(file, func(node ast.Node) bool {
+		call, ok := node.(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+		sel, ok := call.Fun.(*ast.SelectorExpr)
+		if !ok || sel.Sel.Name != "Sleep" {
+			return true
+		}
+		pkg, ok := sel.X.(*ast.Ident)
+		if ok && pkg.Name == "time" {
+			t.Errorf("%s: poll run completion with a bounded ticker", fset.Position(call.Pos()))
+		}
+		return true
+	})
+}
