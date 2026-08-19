ffa350f512131f63107ff8ada2bf4f95578e95b1
Ryan Gaffney <ryanmgaffney@gmail.com>
2026-08-18 23:17:25 -0400

    test(cli): pin lookback duration syntax

--- cmd/sparkwing/duration_flag_test.go
+++ cmd/sparkwing/duration_flag_test.go
@@ -7,15 +7,25 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
-func TestLookbackDurationFlagAcceptsDocumentedDaySuffix(t *testing.T) {
-	fs := flag.NewFlagSet("test", flag.ContinueOnError)
-	since := lookbackDuration(fs, "since", 0, "")
+func TestLookbackDurationFlagAcceptsDocumentedSyntax(t *testing.T) {
+	for _, tc := range []struct {
+		raw  string
+		want time.Duration
+	}{
+		{raw: "7d", want: 7 * 24 * time.Hour},
+		{raw: "2w", want: 2 * 7 * 24 * time.Hour},
+		{raw: "90m", want: 90 * time.Minute},
+	} {
+		t.Run(tc.raw, func(t *testing.T) {
+			fs := flag.NewFlagSet("test", flag.ContinueOnError)
+			since := lookbackDuration(fs, "since", 0, "")
 
-	if err := fs.Parse([]string{"--since", "7d"}); err != nil {
-		t.Fatalf("parse documented --since 7d: %v", err)
-	}
-	if *since != 7*24*time.Hour {
-		t.Fatalf("--since 7d = %s, want %s", *since, 7*24*time.Hour)
+			if err := fs.Parse([]string{"--since", tc.raw}); err != nil {
+				t.Fatalf("parse --since %s: %v", tc.raw, err)
+			}
+			if *since != tc.want {
+				t.Fatalf("--since %s = %s, want %s", tc.raw, *since, tc.want)
+			}
+		})
 	}
 }
-
