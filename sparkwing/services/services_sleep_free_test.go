51c2a09763ab002b07954e501086d951f144a073
Ryan Gaffney <ryanmgaffney@gmail.com>
2026-08-19 18:52:19 -0400

    test(services): reject cleanup sleeps

---
+++ sparkwing/services/services_sleep_free_test.go
@@ -0,0 +1,32 @@
+package services
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"testing"
+)
+
+func TestServiceRegressionsDoNotUseTimeSleep(t *testing.T) {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "services_test.go", nil, 0)
+	if err != nil {
+		t.Fatalf("parse services_test.go: %v", err)
+	}
+
+	ast.Inspect(file, func(node ast.Node) bool {
+		call, ok := node.(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+		sel, ok := call.Fun.(*ast.SelectorExpr)
+		if !ok || sel.Sel.Name != "Sleep" {
+			return true
+		}
+		pkg, ok := sel.X.(*ast.Ident)
+		if ok && pkg.Name == "time" {
+			t.Errorf("services_test.go contains time.Sleep at %s", fset.Position(call.Pos()))
+		}
+		return true
+	})
+}
