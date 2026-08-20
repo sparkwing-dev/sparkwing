89723a333a0a7ab031f7517c9aed5e3e03dc16ae
Ryan Gaffney <ryanmgaffney@gmail.com>
2026-08-19 21:36:20 -0400

    test: reject sleeps in entry exec regressions

---
+++ internal/bincache/entry_exec_sleep_free_test.go
@@ -0,0 +1,33 @@
+//go:build !windows
+
+package bincache
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"testing"
+)
+
+func TestEntryExecRegressionsDoNotSleep(t *testing.T) {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "entry_exec_unix_test.go", nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ast.Inspect(file, func(node ast.Node) bool {
+		call, ok := node.(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+		sel, ok := call.Fun.(*ast.SelectorExpr)
+		if !ok || sel.Sel.Name != "Sleep" {
+			return true
+		}
+		pkg, ok := sel.X.(*ast.Ident)
+		if ok && pkg.Name == "time" {
+			t.Errorf("%s: synchronize through process or file state", fset.Position(call.Pos()))
+		}
+		return true
+	})
+}
