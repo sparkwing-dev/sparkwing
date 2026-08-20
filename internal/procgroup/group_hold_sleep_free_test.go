87d65a47735eef26806eabc68c960c3e75472934
Ryan Gaffney <ryanmgaffney@gmail.com>
2026-08-20 02:40:14 -0400

    test: format cleanup readiness invariant

--- internal/procgroup/group_hold_sleep_free_test.go
+++ internal/procgroup/group_hold_sleep_free_test.go
@@ -13,7 +13,7 @@ import (
 func TestGroupHelperIndefiniteHoldsDoNotUseTimeSleep(t *testing.T) {
 	modes := map[string]bool{
 		"descendant":       false,
-		"ignore-short":      false,
+		"ignore-short":     false,
 		"session-stubborn": false,
 		"session-parked":   false,
 	}
