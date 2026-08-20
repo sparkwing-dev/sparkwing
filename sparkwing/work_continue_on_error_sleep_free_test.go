528133a4488df38f5413c6c66b0a0506a6255396
Ryan Gaffney <ryanmgaffney@gmail.com>
2026-08-20 11:39:40 -0400

    test(sdk): require direct fail-fast cancellation proof

--- sparkwing/work_continue_on_error_sleep_free_test.go
+++ sparkwing/work_continue_on_error_sleep_free_test.go
@@ -29,3 +29,49 @@ func TestWorkFailureHandlingRegressionsDoNotSleep(t *testing.T) {
 		return true
 	})
 }
+
+func TestDefaultFailFastObservesSiblingCancellationDirectly(t *testing.T) {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "work_continue_on_error_test.go", nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var target *ast.FuncDecl
+	for _, decl := range file.Decls {
+		fn, ok := decl.(*ast.FuncDecl)
+		if ok && fn.Name.Name == "TestRunWork_DefaultFailFastCancelsSiblings" {
+			target = fn
+			break
+		}
+	}
+	if target == nil {
+		t.Fatal("TestRunWork_DefaultFailFastCancelsSiblings declaration missing")
+	}
+	observesCancellation := false
+	ast.Inspect(target.Body, func(node ast.Node) bool {
+		switch node := node.(type) {
+		case *ast.CallExpr:
+			sel, ok := node.Fun.(*ast.SelectorExpr)
+			if !ok {
+				return true
+			}
+			pkg, pkgOK := sel.X.(*ast.Ident)
+			if pkgOK && pkg.Name == "time" && sel.Sel.Name == "After" {
+				t.Errorf("%s: observe sibling cancellation directly", fset.Position(node.Pos()))
+			}
+		case *ast.UnaryExpr:
+			sel, ok := node.X.(*ast.SelectorExpr)
+			if !ok {
+				return true
+			}
+			receiver, receiverOK := sel.X.(*ast.Ident)
+			if node.Op == token.ARROW && receiverOK && receiver.Name == "ctx" && sel.Sel.Name == "Done" {
+				observesCancellation = true
+			}
+		}
+		return true
+	})
+	if !observesCancellation {
+		t.Fatal("default fail-fast regression must receive from ctx.Done")
+	}
+}
