33f513215b34daa107e1d3b946d2c5b1c90a10ff
Korey Gambill <korey.gam@gmail.com>
2026-09-09 21:53:49 -0600

    fix(color): share renderer policy and synchronize overrides
    Tickets: BW-2145
    Group: G-64

---
+++ pkg/color/concurrency_test.go
@@ -0,0 +1,28 @@
+package color_test
+
+import (
+	"github.com/sparkwing-dev/sparkwing/pkg/color"
+	"sync"
+	"testing"
+)
+
+func TestConcurrentColorOverrideAndOutput(t *testing.T) {
+	before := color.Enabled()
+	defer color.SetEnabled(before)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for i := range 1000 {
+			color.SetEnabled(i%2 == 0)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for range 1000 {
+			color.Bold("output")
+			color.Enabled()
+		}
+	}()
+	wg.Wait()
+}
